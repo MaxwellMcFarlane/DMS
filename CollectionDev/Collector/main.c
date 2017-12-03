0a593366afe87126fa473e25872c58c4c8afee30
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 15:35:15 -0500

    database new interface test

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -145,7 +145,8 @@ onVoltageChangeHandler(PhidgetVoltageInputHandle ch, void *ctx, double voltage) 
     if(ctx){
         struct dmsBuffer* buffptr = (struct dmsBuffer*) ctx;
 
-        char* msg[100] = "\0"; // 32 hardcode count for below (account for '\0')
+        char* msg[100]; // 32 hardcode count for below (account for '\0')
+        *msg = "\0";
         snprintf(msg, (100*sizeof(char)), "('exampleSensor',%llu,%f),", millisecondsSinceEpoch, voltage);
         snprintf(msg, (sizeof(shar)), "\0");
         buffptr->samples[buffptr->index] = msg;
