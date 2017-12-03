92ddeb80f5a7d1d2de43955f049518da44222499
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 16:44:47 -0500

    added final string

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -327,10 +327,8 @@ int main(int argc, char **argv) {
             clock_gettime(CLOCK_MONOTONIC, &after);
             printf("\n******** PUSH TO DMS ********\n");
 
-            char* final[5000];
-            final = " ";
-            //char* prefix[sizeof(SAMPLE_POSTFIX)] = SAMPLE_POSTFIX;
-            //strcat(&tmp, prefix);*/
+            char final[5000] = "";
+            strcat(final, SAMPLE_PREFIX);
             for(int i = 0; i < buff.index - 1; i++){
                 printf("%s\n", buff.samples[i]);
                 //strcat(&tmp, buff.samples[i]);
