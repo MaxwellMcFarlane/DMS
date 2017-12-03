ab95006700869aeb7a07264b8cd611a317d8892d
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 17:24:55 -0500

    DMS time lets try again

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -227,13 +227,15 @@ int main(int argc, char **argv) {
 
     sqlite3 *db;
     sqlite3_open(DB_PATH,&db);
-    sqlite3_exec(db, "PRAGMA foreign_keys = ON;", 0, 0, ermsg);
+    printf("after open\n");
+    //sqlite3_exec(db, "PRAGMA foreign_keys = ON;", 0, 0, ermsg);
     sqlite3_exec(db, ".separator |", 0, 0, ermsg);
-
+    printf("after exec");
     sqlite3_exec(db,
                  "insert into "
                  "sampletable(sensorid,timestamp,rawdata) "
                  "values('s0',123212,2131231);",0,0,ermsg);
+    printf("after exec 2");
 
 
     //printf("%s\n", ermsg);
