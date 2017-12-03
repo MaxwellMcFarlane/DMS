b7b2a4a68f938e6d518c8c1bc70ef79b9de297a1
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 13:52:45 -0500

    database insert sample

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -228,6 +228,12 @@ int main(int argc, char **argv) {
     sqlite3_exec(db, "PRAGMA foreign_keys = ON;", 0, 0, ermsg);
     sqlite3_exec(db, ".separator |", 0, 0, ermsg);
 
+    sqlite3_exec(db,
+                 "insert into "
+                 "sampletable(sensorid,timestamp,rawdata) "
+                 "values('s0',123212,2131231);",0,0,ermsg);
+
+
     printf("%s\n", ermsg);
     /*
     * Enable logging to stdout
