02c8a7e172530c698e0f9567493e914cbf35e9a7
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 13:13:00 -0500

    fastmode

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -208,7 +208,7 @@ int main(int argc, char **argv) {
         // read HUB and Port
         map[i].hub = 497194;
         map[i].port = i;
-        map[i].samplingPeriod = 100; // in msec
+        map[i].samplingPeriod = 200; // in msec
         // make ch
         res = PhidgetVoltageInput_create(&map[i].ch);
         if (res != EPHIDGET_OK) {
