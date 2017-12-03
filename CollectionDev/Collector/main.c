bb3058f2ff3cc9b163428d27875705ce4fa52c59
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 16:21:45 -0500

    database new interface test - syntax error

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -147,7 +147,6 @@ onVoltageChangeHandler(PhidgetVoltageInputHandle ch, void *ctx, double voltage) 
 
         char msg[100]; // 32 hardcode count for below (account for '\0')
         snprintf(msg, (100*sizeof(char)), "('exampleSensor',%llu,%f),", millisecondsSinceEpoch, voltage);
-        buffptr->samples[buffptr->index] = (char *)malloc(78, sizeof(msg));
         buffptr->samples[buffptr->index] = msg;
         buffptr->index++;
         printf(msg);
