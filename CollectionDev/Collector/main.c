3ae475105e69456b7ba323b4eeb247e8e47f7619
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 10:08:06 -0500

    time update

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -283,14 +283,12 @@ int main(int argc, char **argv) {
     clock_gettime(CLOCK_MONOTONIC, &before);
     printf("***** MAIN LOOP ***** (%i %i)", (long) before.tv_sec, before.tv_nsec);
     while(1){
-        for(size_t i=0; i<10000000; ++i){
-              sink++;
-        }
+
         clock_gettime(CLOCK_MONOTONIC, &after);
         diff.tv_sec = after.tv_sec - before.tv_sec;
         diff.tv_nsec = after.tv_nsec - before.tv_nsec;
         if(diff.tv_sec > 5){
-        printf("***** PUSH TO DMS ***** (%i %i)", (long) before.tv_sec, before.tv_nsec);
+            printf("***** PUSH TO DMS ***** (%i %i)", (long) before.tv_sec, before.tv_nsec);
 
         }
     }
