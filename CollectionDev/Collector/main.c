d7ae98157201fcc85faa0754b129946fe5d7b16e
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 18:18:51 -0500

    DMS time lets try again

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -226,8 +226,8 @@ int main(int argc, char **argv) {
     char *ermsg = 0;
     sqlite3 *db;
     sqlite3_open(DB_PATH, &db);
-    rc = sqlite3_exec(db, "PRAGMA foreign_keys = ON;", 0, 0, &ermsg);
-    rc = sqlite3_exec(db, ".separator |", 0, 0, &ermsg);
+    sqlite3_exec(db, "PRAGMA foreign_keys = ON;", 0, 0, &ermsg);
+    sqlite3_exec(db, ".separator |", 0, 0, &ermsg);
     printf("after exec");
     sqlite3_exec(db,
                  "insert into "
