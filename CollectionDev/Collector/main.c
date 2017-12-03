ec535aca08092253067564187b7759615279ef90
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 12:45:49 -0500

    added file dumping - removed macro

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -300,7 +300,7 @@ int main(int argc, char **argv) {
         clock_gettime(CLOCK_MONOTONIC, &after);
         diff.tv_sec = after.tv_sec - before.tv_sec;
         diff.tv_nsec = after.tv_nsec - before.tv_nsec;
-        if(((int) diff.tv_sec) > DMSBUFFER_SMPL_TIME || buff.numSample > DMSBUFFER_SMPLTHRESHOLD){
+        if(((int) diff.tv_sec) > 5 || buff.numSample > DMSBUFFER_SMPLTHRESHOLD){
             before = after;
             clock_gettime(CLOCK_MONOTONIC, &after);
             printf("******** PUSH TO DMS ********\n");
