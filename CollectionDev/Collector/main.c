8f1dca6061797f6a101e981073aee0e641154569
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 21:24:30 -0500

    It finally works

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -207,7 +207,7 @@ int main(int argc, char **argv) {
     buff.record = 0;
 
     // use readPipe to instantiate Map Sensor Architecture
-    int numbSensors = 1;
+    int numbSensors = 4;
     struct Sensor map[numbSensors];
     PhidgetReturnCode res;
     const char *errs;
@@ -216,7 +216,7 @@ int main(int argc, char **argv) {
         // read HUB and Port
         map[i].hub = 497194;
         map[i].port = i;
-        map[i].samplingPeriod = 5000; // in msec
+        map[i].samplingPeriod = 1000 + (184 * i); // in msec
         // make ch
         res = PhidgetVoltageInput_create(&map[i].ch);
         if (res != EPHIDGET_OK) {
