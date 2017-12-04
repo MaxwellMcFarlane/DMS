a94e6effbb3e496662c6d49cedb4a80ec7af3761
Hayden Dodge (pc) <dodgeh@lafayette.edu>
2017-12-03 23:45:28 -0500

    pulled old version of COllector

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -151,7 +151,7 @@ onVoltageChangeHandler(PhidgetVoltageInputHandle ch, void *ctx, double voltage) 
             int index = buffptr->index;
             buffptr->index++;
             buffptr->samples[index] = msg;
-            printf("%s\n", msg);
+            // printf("%s\n", msg);
         }
     }
 
@@ -314,7 +314,7 @@ int main(int argc, char **argv) {
     volatile unsigned sink;
     struct timespec before, after, diff;
     clock_gettime(CLOCK_MONOTONIC, &before);
-    printf("******** MAIN LOOP ********\n");
+    //printf("******** MAIN LOOP ********\n");
     buff.record = 1;
     while(1){
 
@@ -326,7 +326,7 @@ int main(int argc, char **argv) {
             before = after;
             clock_gettime(CLOCK_MONOTONIC, &after);
 
-            // printf("\n******** PUSH TO DMS ********\n");
+            printf("\n******** PUSH TO DMS ********\n");
 
             // multiple individual insert
             sqlite3_open(DB_PATH, &db);
