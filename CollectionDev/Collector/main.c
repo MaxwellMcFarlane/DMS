26437acf16ab2c7d8970c5915d738bbbf7634097
Hayden Dodge (rpi) <dodgeh@lafayette.edu>
2017-12-04 05:43:19 +0000

    Found correct file. Well actually it may have actually been correct. Turns out the scada table had changed but the other terminal windows looking at the scada.db was not reflecting this change. This file will WORK with the updated scada.db. Thank you. Sorry it is 12:43 AM.

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -219,7 +219,7 @@ int main(int argc, char **argv) {
         // read HUB and Port
         map[i].hub = 497194;
         map[i].port = i;
-        map[i].samplingPeriod = 1500 + (300 * i); // in msec
+        map[i].samplingPeriod = 1000; // in msec
         // make ch
         res = PhidgetVoltageInput_create(&map[i].ch);
         if (res != EPHIDGET_OK) {
