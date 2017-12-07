e41ef9ae9f2e9fab20af42636a2da04ef9195d3b
mcfarlmt <mcfarlmt@lafayette.edu>
2017-12-07 12:12:04 -0500

    organize collection

--- CollectionDev/Collector/main.c
+++ CollectionDev/Collector/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <phidget22.h>
 
 #ifndef _WIN32
@@ -323,14 +322,13 @@ int main(int argc, char **argv) {
     clock_gettime(CLOCK_MONOTONIC, &before);
     printf("******** MAIN LOOP ********\n");
     buff.record = 1;
-    while(1){
-
+    while(1){        
         clock_gettime(CLOCK_MONOTONIC, &after);
         diff.tv_sec = after.tv_sec - before.tv_sec;
         diff.tv_nsec = after.tv_nsec - before.tv_nsec;
         if(((int) diff.tv_sec) > DMSBUFFER_SMPL_TIME || buff.index > MAXBUFFERED_SAMPLES - 5){
-            before = after;
-            clock_gettime(CLOCK_MONOTONIC, &after);
+            before = after;            
+            clock_gettime(CLOCK_MONOTONIC, &after);            
             printf("\n******** PUSH TO DMS ********\n");
 
             char final[200] = "";
@@ -359,7 +357,6 @@ int main(int argc, char **argv) {
                 printf("err: %s\n", ermsg);
             }
             sqlite3_close(db);
-
             // console print
             //printf("%s", &buff.samples);
             printf("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n\n");
